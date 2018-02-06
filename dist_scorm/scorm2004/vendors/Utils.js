0d51b1374ca05ab10210100bafde48d9986cf1fd
ebarra <ebarra@dit.upm.es>
2018-02-06 14:23:30 +0100

    scorm package

--- dist_scorm/scorm2004/vendors/Utils.js
+++ dist_scorm/scorm2004/vendors/Utils.js
@@ -1,3 +1,5 @@
+import {TRANSLATIONS} from '../constants/constants.js';
+
 var next_objective_id = 1;
 
 export function objective(options){
@@ -12,7 +14,7 @@ export function objective(options){
   var objective = Object.assign({}, defaults, options);
 
   objective.progress_measure = Math.max(0,Math.min(1,objective.progress_measure));
-  
+
   if(typeof objective.score == "number"){
     objective.score = Math.max(0,Math.min(1,objective.score));
     if(typeof objective.accomplished_score == "number"){
@@ -22,4 +24,20 @@ export function objective(options){
 
   next_objective_id += 1;
   return objective;
+}
+
+export function translate(text){
+  if(TRANSLATIONS[text]){
+    return TRANSLATIONS[text];
+  } else {
+    return text;
+  }
+}
+
+export function translateTime(text){
+  if(TRANSLATIONS[text]){
+    return TRANSLATIONS[text];
+  } else {
+    return text.replace("second", "segundo").replace("minute", "minuto").replace("hour", "hora").replace("day", "día").replace("week", "semana").replace("months", "meses").replace("month", "mes").replace("year", "año").replace("century", "siglo").replace("centuries", "siglos");
+  }
 }
